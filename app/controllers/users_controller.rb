5894e349c5b2d3e63f86d415dd462819be607047
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-28 13:10:36 -0500

    User can delete their account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+      redirect_to root_url, notice: "Bye! Your account has been successfully
+                                    cancelled. We hope to see you again soon."
+    end
+  end
 end
