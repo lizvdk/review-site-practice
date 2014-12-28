317b439ff12705fd08a726cdb329a0196ce52d1e
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-28 16:45:59 -0500

    Trees display in correct order

---
+++ spec/features/view_all_trees_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'view all trees', %Q(
+  As a user
+  I want to view recently posted trees
+  So that I can see other people's trees
+  ) do
+
+  # Acceptance Criteria:
+  # * I must see the title of each tree
+  # * I must see trees listed in order, most recently posted first
+  # * Tree titles link to their show page
+  # * I must see if a tree was posted by an arborist or a candidate
+
+  scenario 'trees display in order' do
+    trees = FactoryGirl.create_list(:tree, 3)
+    visit trees_path
+
+    page.should have_selector('ul#trees li:nth-child(1)', text: trees[2].title)
+    page.should have_selector(
+                          'ul#trees li:nth-child(1)', text: trees[2].user.role)
+    page.should have_selector('ul#trees li:nth-child(2)', text: trees[1].title)
+    page.should have_selector(
+                          'ul#trees li:nth-child(2)', text: trees[1].user.role)
+    page.should have_selector('ul#trees li:nth-child(3)', text: trees[0].title)
+    page.should have_selector(
+                          'ul#trees li:nth-child(3)', text: trees[0].user.role)
+  end
+end
